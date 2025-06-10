8d5cf42e2616b30a5884c4f22e15cd62ba551c0c
Tomasz Gil <tomaszgil.dev@gmail.com>
2025-06-10 15:30:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Theme } from "@radix-ui/themes";
+
+function App() {
+  return (
+    <Theme>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </Theme>
+  );
+}
+
+export default App;
