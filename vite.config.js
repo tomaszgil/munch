7c054c75d32f09144680e06aeba03ea74f20a99f
Tomasz Gil <tomaszgil.dev@gmail.com>
2025-06-15 17:43:42 +0200

    Add basic PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "logo192.png", "logo512.png"],
+      manifest: false,
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
-  }
+  },
 });
